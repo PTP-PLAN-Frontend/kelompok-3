22da7f574f1cc30ba80d5cd51617ecdf5d84b84e
arsitanrfzh <arsita.nurfauziah@gmail.com>
2021-05-02 14:06:00 +0700

    memperbaiki teks cultivation

--- src/component/Jahemerah.js
+++ src/component/Jahemerah.js
@@ -6,15 +6,53 @@ export default function JaheMerahCultivation () {
   return (
     <Container>
     <div className='jahemerah-main'>
-      <center><h1>Penanaman Rimpang Jahe</h1></center>
+      <center><h1>Budidaya Jahe Merah</h1></center>
       <center><img alt='jahe-merah' src="http://3.bp.blogspot.com/-WnXySINBfvo/UuPbbZPwKMI/AAAAAAAAA9g/0HUspdG63I8/s1600/tanaman+jahe.jpg"></img></center>
+      <br></br>
       <p align="justify">
-      Setelah benih siap ditanam, masukkan rimpang ke dalam karung atau media, banyaknya jumlah rimpang tergantung besar
-      rimpang dan jumlah mata tunasnya, namun biasanya cukup menggunakan 5-10rimpang saja perkarungnya.
-      Usahakan menanam benih jahe secara merata agar nantinya jahe mudah berkembangnya dan timbun dengan campuran tanah yang
-      telah diramu sebelumnya. Setelah itu siram dengan air secukupnya dan letakkan karung yang telah diisi oleh benih ke daerah yang ternaungi dari sinar matahari langsung sampai usia 2-3 bulan supaya daunnya tidak menguning.
+      <p>1. Penyiapan Media Tanam</p>
+      <p>Media tanam seperti karung juga mempunyai kelemahan seperti mudah rusak atau bolong, namun dengan penanganan yang tepat hal tersebut dapat dihindari,
+        selain menggunakan karung ada juga yang menggunakan polybag, namun permasalahan yang ditemukan adalah polybag kurang tinggi dan adalagi dengan media
+        keranjang dan pot, namun biayanya tentu lebih mahal.</p>
+        
+      <p>Setelah menentukan media apa yang akan kita pakai selanjutnya yang perlu kita lakukan adalah menyiapkan tanah untuk isian media tadi atau tempat tanamnya
+        jahe nanti, isian yang digunakan adalah tanah, pasir, dan pupuk organik dengan perbandingan 1:1:1 atau 1:1:2.</p>
+      <p>Tanah yang baik adalah tanah yang gembur dan subur. Gembur artinya remah dan komposisi liat, pasir, dan debunya seimbang.
+        Subur berarti banyak kandungan unsur haranya. Jika tanah tersebut tidak tidak tersedia maka diperlukan perlakuan lain agar tanah dapat digunakan, seperti
+        penambahan pasir dan pupuk.</p>
       
-      Sumber: http://ilmujahe.blogspot.com/2014/01/budidaya-jahe-merah-dengan-karung.html?spref=pi
+      <p>Pasir diperlukan jika tanah yang digunakan mengandung fraksi liat yang cukup tinggi. Pasir yang digunakan adalah pasir ladu atau pasir yang bercampur
+        dengan lumpur. Selain murah, pasir ini juga masih mengandung bahan-bahan mineral endapan.</p>
+      
+      <p>Pupuk organik bisa menggunakan pupuk kandang, pupuk kompos atau bokashi. Meskipun menggunakan pupuk kandang, akan lebih bagus jika pupuk kandang yang
+        telah dihancurkan dan difermentasi sehingga lebih cepat diserap oleh akar tanaman. Seluruh media tersebut dicampur merata sambil dibersihkan dari
+        benda-benda yang mengganggu, misalnya plastik, batu atau benda lainnya. Kemudian media pengisi dimasukkan ke dalam karung atau media yang telah disiapkan.
+        Pengisian karung atau polybag cukup ¼ bagian saja, karena selama pertumbuhan tanaman nanti, akan dilakukan penambahan pupuk organik.</p>
+        
+      <p>2. Pembibitan Jahe</p>
+      <p>Bibit yang ideal adalah bibit telah berumur tua, besar, warnanya cerah dan tidak ada lukanya, untuk itu saya sarankan untuk memcari bibit dari petani
+        langsung bukan di pasar demi menjamin kualitas bibit jahe yang akan kita tanam.</p>
+        
+      <p>Setelah mendapatkan bibit yang kita cari, selanjutnya untuk menghindari penyakit yang disebabkan jamur perlu dilakukan perendaman bibit kedalam larutan
+        fungisida (misalnya Dithane M-45) selama lebih kurang 15 menit.</p>
+        
+      <p>Selanjutnya bibit di semai di tempat yang lembab dan tidak terkena matahari ataupun hujan secara langsung, bisa di kumbung atau gudang dengan dialasi
+        alang-alang atau jerami, kemudian letakkan  benih secara merata di atas alang-alang atau jerami tadi dan tutup kembali dengan jerami atau alang-alang.</p>
+        
+      
+      <p>Perlu diperhatikan untuk mengecek bibit setiap hari guna mengontrol kelembaban benih dengan menyiraminya dengan sedikit air secara merata, setelah lebih
+        kurang 2 minggu akan tumbuh tunas-tunas kecil berwarna putih dengan ukuran 3 cm atau lebih, dan itu tandanya benih siap ditanam ke media.</p>
+      
+      <p>3. Penanaman Rimpang Jahe</p>
+      <p>Setelah benih siap ditanam, masukkan rimpang ke dalam karung atau media, banyaknya jumlah rimpang tergantung besar rimpang dan jumlah mata tunasnya,
+        namun biasanya cukup menggunakan 5-10rimpang saja perkarungnya.</p>
+
+      <p>Usahakan menanam benih jahe secara merata agar nantinya jahe mudah berkembangnya dan timbun dengan campuran tanah yang telah diramu sebelumnya.
+        Setelah itu siram dengan air secukupnya dan letakkan karung yang telah diisi oleh benih ke daerah yang ternaungi dari sinar matahari langsung
+        sampai usia 2-3 bulan supaya daunnya tidak menguning.</p>
+        
+      <small className='text-muted'>Sumber: http://ilmujahe.blogspot.com/2014/01/budidaya-jahe-merah-dengan-karung.html?spref=pi
+      </small>
       </p> 
     </div>
     </Container>
